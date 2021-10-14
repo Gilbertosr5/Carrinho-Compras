bb7385adb38c3a93bcf254ce46feb2a23fc268c6
Gilberto <gillbertosr5@gmail.com>
2021-10-14 12:02:40 -0300

    Aula 09

--- js/script.js
+++ js/script.js
@@ -3,5 +3,23 @@ const cs = (el)=>document.querySelectorAll(el);
 
 modelsJson.map((item, index)=>{
     let modelsItem = c('.models .models-item').cloneNode(true);
+    modelsItem.setAttribute('data-key', index)
+    modelsItem.querySelector('.models-item--img img').src = item.img;
+    modelsItem.querySelector('.models-item--price').innerHTML = `R$ ${item.price[0].toFixed(2)}`;
+    modelsItem.querySelector('.models-item--name').innerHTML = item.name;
+    modelsItem.querySelector('.models-item--desc').innerHTML = item.description;
+
+    modelsItem.querySelector('a').addEventListener('click', (e)=>{
+        e.preventDefault() 
+        let key = e.target.closest('.models-item').getAttribute('data-key')
+        c('.modelsBig img').src = modelsJson[key].img;
+        c('.modelsInfo h1').innerHTML = modelsJson[key].name;
+        c('.modelsInfo--desc ').innerHTML = modelsJson[key].description;
+        c('.modelsWindowArea').style.opacity = 0;
+        c('.modelsWindowArea').style.display = 'flex';
+        setTimeout(()=>{
+            c('.modelsWindowArea').style.opacity = 1;
+        },200)
+    })
     c('.models-area').append(modelsItem);
 });
